cb967d0328c86140f5a9f50c623ffad165f1eeb4
Clement Lecocq <hello@clementlecocq.com>
2022-07-12 09:49:45 +0200

    FIRST COMMIT

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+/* Pas de code dans les routes
+faire des controllers
+*/
+
+module.exports = router;
