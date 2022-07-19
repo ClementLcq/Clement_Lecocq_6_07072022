ccb74f410a0a34b6bf87584586ed82dc727bea66
Clement Lecocq <hello@clementlecocq.com>
2022-07-19 14:00:15 +0200

    Création fichier model Sauce

---
+++ models/Sauce.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+// Création du schéma Sauce attendu par le frontend
+
+const sauceSchema = mongoose.Schema({
+    userId: { type: String, required: true },
+    name: { type: String, required: true },
+    manufacturer: { type: String, required: true },
+    description: { type: String, required: true },
+    mainPepper: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    heat: { type: Number, required: true },
+    likes: { type: Number, required: false, default: 0 },
+    dislikes: { type: Number, required: false, default: 0 },
+    usersLiked: { type: [String] },
+    usersDisliked: { type: [String] },
+});
+
+module.exports = mongoose.model("sauces", sauceSchema);
