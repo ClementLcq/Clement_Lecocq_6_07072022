17344b0f5438d40c65863fc28c67ae4fb9e0fa59
Clement Lecocq <hello@clementlecocq.com>
2022-07-19 17:42:16 +0200

    Création d'un controller et d'une route pour like/dislike

--- controllers/sauces.controller.js
+++ controllers/sauces.controller.js
@@ -16,7 +16,7 @@ exports.createSauce = (req, res, next) => {
     sauce.save()
         .then(() => res.status(201).json({ message: 'Objet enregistré' }))
         .catch(error => res.status(400).json({ error }));
-}
+};
 
 // Modifier une sauce
 
@@ -43,7 +43,7 @@ exports.modifySauce = (req, res, next) => {
             });
     }
 
-}
+};
 
 // Supprimer une sauce
 
@@ -55,10 +55,10 @@ exports.deleteSauce = (req, res, next) => {
                 Sauce.deleteOne({ _id: req.params.id })
                     .then(() => res.status(200).json({ message: 'Objet supprimé' }))
                     .catch(error => res.status(400).json({ error }));
-            })
+            });
         })
         .catch(error => res.status(500).json({ error }));
-}
+};
 
 // Afficher toutes les sauces
 
@@ -66,12 +66,77 @@ exports.displaySauces = (req, res, next) => {
     Sauce.find()
         .then(sauces => res.status(200).json(sauces))
         .catch(error => res.status(400).json({ error }));
-}
+};
 
-// Afficher une sauce
+// Afficher une sauce avec _id
 
 exports.getOneSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
         .then(sauce => res.status(200).json(sauce))
         .catch(error => res.status(404).json({ error }));
+};
+
+// Evaluer une sauce
+
+// Tutos : https://www.youtube.com/watch?v=3Uu11DrlEig
+// https://www.youtube.com/watch?v=w8bo1DohwrA
+// https://www.youtube.com/watch?v=RagSZzL2wF0
+// https://www.youtube.com/watch?v=4TRMcvRua0I
+
+exports.evaluateSauce = (req, res, next) => {
+    // Récupération de la sauce avec params.id
+    Sauce.findOne({ _id: req.params.id })
+        .then(sauce => {
+            switch (req.body.like) {
+                // Cas #1 : si l'utilisateur dislike la sauce 
+                case -1:
+                    Sauce.updateOne({ _id: req.params.id }, {
+                            $inc: { dislikes: 1 },
+                            $push: { usersDisliked: req.body.userId },
+                            _id: req.params.id
+                        })
+                        .then(() => res.status(201).json({ message: 'Mince, nous n\'aimez plus cette sauce' }))
+                        .catch(error => res.status(400).json({ error }))
+                    break;
+
+                    // Cas #2 : si la valeur like ou dislike est différente de 0
+                case 0:
+                    // Cas #2-A : si la sauce est déjà likée
+                    if (sauce.usersLiked.find(user => user === req.body.userId)) {
+                        Sauce.updateOne({ _id: req.params.id }, {
+                                $inc: { likes: -1 },
+                                $pull: { usersLiked: req.body.userId },
+                                _id: req.params.id
+                            })
+                            .then(() => res.status(201).json({ message: 'Votre avis a bien été modifié, merci' }))
+                            .catch(error => res.status(400).json({ error }))
+                    }
+
+                    // Cas #2-B : Si la sauce est déjà dislikée
+                    if (sauce.usersDisliked.find(user => user === req.body.userId)) {
+                        Sauce.updateOne({ _id: req.params.id }, {
+                                $inc: { dislikes: -1 },
+                                $pull: { usersDisliked: req.body.userId },
+                                _id: req.params.id
+                            })
+                            .then(() => res.status(201).json({ message: 'Votre avis a bien été modifié, merci' }))
+                            .catch(error => res.status(400).json({ error }));
+                    }
+                    break;
+
+                    // Cas #3 : si l'utilisateur like la sauce
+                case 1:
+                    Sauce.updateOne({ _id: req.params.id }, {
+                            $inc: { likes: 1 },
+                            $push: { usersLiked: req.body.userId },
+                            _id: req.params.id
+                        })
+                        .then(() => res.status(201).json({ message: 'Super, vous adorez cette sauce!' }))
+                        .catch(error => res.status(400).json({ error }));
+                    break;
+                default:
+                    return res.status(500).json({ error });
+            }
+        })
+        .catch(error => res.status(500).json({ error }))
 }
--- routes/sauces.routes.js
+++ routes/sauces.routes.js
@@ -13,9 +13,10 @@ const multer = require("../middleware/multerFunctions");
 
 router.post("/", auth, multer, sauceCtrl.createSauce);
 router.get("/:id", auth, sauceCtrl.getOneSauce);
-router.delete("/:id", sauceCtrl.deleteSauce); //Ajouter une fonction pour vérifier si c'est bien le créateur qui modifie la sauce
-router.put("/:id", multer, sauceCtrl.modifySauce); //Ajouter une fonction pour vérifier si c'est bien le créateur qui modifie la sauce
+router.delete("/:id", sauceCtrl.deleteSauce); //Ajouter une fonction pour vérifier si c'est bien le créateur qui supprime la sauce ?
+router.put("/:id", multer, sauceCtrl.modifySauce); //Ajouter une fonction pour vérifier si c'est bien le créateur qui modifie la sauce ?
 router.get("/", auth, sauceCtrl.displaySauces);
+router.post("/:id/like", auth, sauceCtrl.evaluateSauce);
 
 
 module.exports = router;
