a6126fba3c879c1e94d70f2e70f628625314b7d3
Clement Lecocq <hello@clementlecocq.com>
2022-07-19 12:08:40 +0200

    Ajout d'un schéma de validation du mdp avec password-validator

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 const authCtrl = require('../controllers/auth.controller');
 
+// Import du validateur de mot de passe
+
+const password = require('../middleware/passwordValidator.middleware');
+
 /* GET users listing. */
-router.post('/signup', authCtrl.signup);
+router.post('/signup', password, authCtrl.signup);
 router.post('/login', authCtrl.login);
 
 module.exports = router;
